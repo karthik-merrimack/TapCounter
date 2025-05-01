f7a2263514e370993235da9cd8fa42308e6228cc
karthik-merrimack <thirugnanask@merrimack.edu>
2025-04-30 20:42:22 -0500

    Initial Commit

---
+++ TapCounter/TapCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapCounterApp.swift
+//  TapCounter
+//
+//  Created by Karthik Thirugnanasampantham on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TapCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TapCounterTests/TapCounterTests.swift
@@ -0,0 +1,16 @@
+//
+//  TapCounterTests.swift
+//  TapCounterTests
+//
+//  Created by Karthik Thirugnanasampantham on 4/30/25.
+//
+
+import Testing
+
+struct TapCounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
